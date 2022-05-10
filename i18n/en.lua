5f14730aaab8f5d4958ccbc877d550f6127e3a99
IsJustaGhost <56104601+IsJustaGhost@users.noreply.github.com>
2022-05-09 19:56:04 -0700

    Upddated strings

--- i18n/en.lua
+++ i18n/en.lua
@@ -6,10 +6,10 @@ local strings = {
 	SI_BMU_GAMEPAD_CATEGORY_DELVES		= "Delves and open Dungeons",
 	
 	SI_BMU_GAMEPAD_PAN					= "Focus in on map pin",
-	SI_BMU_GAMEPAD_PAN_TOOLTIP			= "Focus in on map pin",
+	SI_BMU_GAMEPAD_PAN_TOOLTIP			= "Enabled: If a destination has a map pin, on selection the map will auto-focus in on the pin.",
 	
 	SI_BMU_GAMEPAD_PAN_TO_GROUP			= "Focus in on group member",
-	SI_BMU_GAMEPAD_PAN_TO_GROUP_TOOLTIP	= "Focus in on group member",
+	SI_BMU_GAMEPAD_PAN_TO_GROUP_TOOLTIP	= "Enabled: on selection of a group member, the map will auto-focus in on them.",
 	
 	SI_BMU_GAMEPAD_MANAGE_FAVORITES		= 'Manage Favorites',
 }
